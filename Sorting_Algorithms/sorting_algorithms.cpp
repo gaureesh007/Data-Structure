68ceec9aa667218daf4264a45c8763b099e8f70c
Gaureesh <mac@Gaureeshs-Mac.local>
2026-06-04 22:48:15 +0530

    written quicksort in sorting algo

--- Sorting_Algorithms/sorting_algorithms.cpp
+++ Sorting_Algorithms/sorting_algorithms.cpp
@@ -44,12 +44,35 @@ void selectionSort(int A[] , int size){
     }
 }
 
+//QuickSort
+
+int partition(int A[],int l,int h ){
+    int Pivot = A[l];
+    int i = l,j=h;
+    do{
+        do{i++;}while(A[i]<=Pivot);
+        do{j--;}while(A[j]>Pivot);
+        if(i<j) swap(&A[i],&A[j]);
+    }while(i<j);
+    swap(&A[l],&A[j]);
+    return j;
+
+}
+void quickSort(int A[],int l, int h){
+    int j;
+    if(l<h){
+        j=partition(A,l,h);
+        quickSort(A,l,j);
+        quickSort(A,j+1,h);
+    }
+}
 int main(){
     int A[10] = {1,3,6,4,3,2,7,8,1,9};
     int size = 10;
     // bubbleSort(A,10);
     // insertionSort(A,10);
-    selectionSort(A,10);
+    // selectionSort(A,10);
+    quickSort(A,0,9);
     for(int i = 0; i<size; i++){
         cout<<A[i]<<" ";
     }
